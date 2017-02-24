b62983d747bedfb6084ba7512a32e2782964e279
Aubrey Goodman <aubrey.goodman@gmail.com>
2017-02-23 20:22:24 -0800

    Initial Commit

---
+++ BLEProducer-OSX/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BLEProducer-OSX
+//
+//  Created by Aubrey Goodman on 2/23/17.
+//  Copyright © 2017 Aubrey Goodman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
